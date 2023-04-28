284f9ae13add6d37e5b99e1dcc2e2fc9448c13df
Edson Manners <edson.manners@gmail.com>
2023-04-28 16:48:42 -0400

    Full running code

--- roboControl.py
+++ roboControl.py
@@ -6,3 +6,19 @@ if __name__ == "__main__":
     # Create a Create2 bot
     port = '/dev/ttyUSB0'
     baud = { 'default': 115200 }
+
+    bot = pycreate2.Create2(port=port, baud=baud['default'])
+
+    # Define a movement path
+    path = [
+        [  20,  20, 3, 'for'],
+        [ -20, -20, 3, 'back'],
+        [   0,   0, 1, 'stop'],
+        [  10,   0, 2, 'rite'],
+        [   0,  10, 4, 'left'],
+        [  10,   0, 2, 'rite'],
+        [   0,   0, 1, 'stop'],
+    ]
+
+    bot.start()
+    bot.stop()
