0a30de6be943c0e5b16039e6d53718f682570103
Edson Manners <edson.manners@gmail.com>
2023-08-31 22:06:10 +0100

    Adding pi cam v3 code

--- PiCamera/robot.py
+++ PiCamera/robot.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+#
 # https://picamera.readthedocs.io/en/release-1.13/recipes1.html#recipes1
 # https://core-electronics.com.au/guides/pan-tilt-hat-raspberry-pi/
 # https://learn.pimoroni.com/article/assembling-pan-tilt-hat
 
 from time import sleep
 
-from picamera import PiCamera
-import pantilthat
+from picamera2 import Picamera2
+#import pantilthat
 
 
-camera = PiCamera()
+camera = Picamera2()
 
 camera.resolution = (1024, 768)
 camera.start_preview()
