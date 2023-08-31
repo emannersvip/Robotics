b4c485ac4e2a878a704575c04e4bfcc8142d684f
Edson Manners <edson.manners@gmail.com>
2023-08-31 22:53:50 +0100

    file path updates

--- Puppet/manifests/eom_motion.pp
+++ Puppet/manifests/eom_motion.pp
@@ -2,13 +2,13 @@ file {'motion_service':
   ensure  => present,
   path    => '/lib/systemd/system/motion.service',
   #content => template('motion.service.erb'),
-  source  => 'file:///motion.service',
+  source  => 'file:motion.service',
 }
 
 file {'motion_binary':
   ensure => present,
   path   => '/usr/local/bin/motion',
-  source => 'puppet:///PiSurveillanceCamera.py',
+  source => '../files/PiSurveillanceCamera.py',
 }
 
 service {'motion':
