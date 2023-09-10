d2a95d3b5237f622ecd235d4626f7021918cd24b
Edson Manners <edson.manners@gmail.com>
2023-09-10 10:18:55 -0400

    Adding the DB

--- Testing/ehpc_cli.py
+++ Testing/ehpc_cli.py
@@ -1,7 +1,45 @@
 #!/usr/bin/python3
 
+# https://click.palletsprojects.com/en/7.x/
+# https://geekyhumans.com/how-to-create-cli-in-python/
+
+from colorama import Fore, Back, Style
 from ehpc_cluster import Cluster
 
-pcluster = Cluster('picamera')
+import click
+import sqlite3
 
+# Initialize the environment
+sqlite3.connect('ehpc.db')
 
+@click.group()
+def ehpc_cli():
+    """The eHPC CLI"""
+
+@ehpc_cli.group()
+def cluster():
+    """All comannds related to clusters"""
+    # If at least one cluster exists operate normally 
+    # Else badger/notify the user of the cluster's inactive status and remediation steps.
+    #   I.e.Create a login and scheduler node.
+
+@ehpc_cli.group()
+def login():
+    """All comannds related to logins"""
+
+@ehpc_cli.group()
+def compute():
+    """All comannds related to computes"""
+
+@ehpc_cli.group()
+def scheduler():
+    """All comannds related to schedulers"""
+
+@ehpc_cli.group()
+def status():
+    """All comannds related to Global system status"""
+
+#pcluster = Cluster('picamera')
+
+if __name__ == '__main__':
+    ehpc_cli(prog_name='ehpc')
