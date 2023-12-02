6d3dfa1f05a32459393d66f672dffec1f4e077ed
Edson Manners <edson.manners@gmail.com>
2023-12-02 04:26:04 +0000

    Updates - ncurses not working

--- PiCamera/eom_pantilt_sunfounder.py
+++ PiCamera/eom_pantilt_sunfounder.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import curses
+
 from servo import Servo
 from time import sleep
 
@@ -9,12 +11,39 @@ tilt = Servo(pin=12, max_angle=30, min_angle=-90)
 #tilt = Servo(pin=12)
 
 def main():
-    while True:
-        print(f"Bob is here")
-        pan.set_angle(10)
-        sleep(2)
-        tilt.set_angle(10)
-        #pass
+    #while True:
+    #    print(f"Bob is here")
+    #    pan.set_angle(10)
+    #    sleep(2)
+    #    tilt.set_angle(10)
+    screen = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    screen.keypad(True)
+
+    screen.addstr(1,  2, 'Press    to QUIT' )
+    screen.addstr(1,  8, '\'Q\'', curses.A_BLINK )
+    screen.addstr(2,  2, 'Press \'D\' to get DEBUG info' )
+
+    screen.addstr(5,  2, '===============================================')
+    screen.addstr(6,  2, '#                Pan Tilt Controls            #')
+    screen.addstr(7,  2, '# URL: ssh://pi-robot-2.edsonmanners.com:8000 #')
+    screen.addstr(8,  2, '#                                             #')
+    screen.addstr(9,  2, '#                                             #')
+    screen.addstr(10, 2, '#                    Up                       #')
+    screen.addstr(11, 2, '#               Left    Right                 #')
+    screen.addstr(12, 2, '#                   Down                      #')
+    screen.addstr(13, 2, '#                                             #')
+    screen.addstr(14, 2, '#                                             #')
+    screen.addstr(15, 2, '#                                             #')
+    screen.addstr(16, 2, '# URL: http://192.168.68.24:8888              #')
+    screen.addstr(17, 2, '# URL: http://192.168.68.24:8888              #')
+    screen.addstr(18, 2, '===============================================')
+    sleep(5)
+    #--
+    curses.nocbreak(); screen.keypad(0); curses.echo()
+    curses.endwin()
+    return
 
 if __name__ == '__main__':
     try:
