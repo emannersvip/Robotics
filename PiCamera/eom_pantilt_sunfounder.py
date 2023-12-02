633352b1f62e87bf54e1eeb96e5cf576ea49e421
Edson Manners <edson.manners@gmail.com>
2023-12-02 16:11:49 +0000

    sunfounder updates

--- PiCamera/eom_pantilt_sunfounder.py
+++ PiCamera/eom_pantilt_sunfounder.py
@@ -2,6 +2,7 @@
 
 import curses
 
+from curses import wrapper
 from servo import Servo
 from time import sleep
 
@@ -10,43 +11,66 @@ pan  = Servo(pin=13, max_angle=90, min_angle=-90)
 tilt = Servo(pin=12, max_angle=30, min_angle=-90)
 #tilt = Servo(pin=12)
 
-def main():
+def main(stdscr):
     #while True:
     #    print(f"Bob is here")
     #    pan.set_angle(10)
     #    sleep(2)
     #    tilt.set_angle(10)
-    screen = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    screen.keypad(True)
+    stdscr.clear()
 
-    screen.addstr(1,  2, 'Press    to QUIT' )
-    screen.addstr(1,  8, '\'Q\'', curses.A_BLINK )
-    screen.addstr(2,  2, 'Press \'D\' to get DEBUG info' )
+    stdscr.addstr(1,  2, 'Press     to QUIT' )
+    stdscr.addstr(1,  8, '\'Q\'', curses.A_BLINK )
+    stdscr.addstr(2,  2, 'Press \'D\' to get DEBUG info' )
 
-    screen.addstr(5,  2, '===============================================')
-    screen.addstr(6,  2, '#                Pan Tilt Controls            #')
-    screen.addstr(7,  2, '# URL: ssh://pi-robot-2.edsonmanners.com:8000 #')
-    screen.addstr(8,  2, '#                                             #')
-    screen.addstr(9,  2, '#                                             #')
-    screen.addstr(10, 2, '#                    Up                       #')
-    screen.addstr(11, 2, '#               Left    Right                 #')
-    screen.addstr(12, 2, '#                   Down                      #')
-    screen.addstr(13, 2, '#                                             #')
-    screen.addstr(14, 2, '#                                             #')
-    screen.addstr(15, 2, '#                                             #')
-    screen.addstr(16, 2, '# URL: http://192.168.68.24:8888              #')
-    screen.addstr(17, 2, '# URL: http://192.168.68.24:8888              #')
-    screen.addstr(18, 2, '===============================================')
-    sleep(5)
-    #--
-    curses.nocbreak(); screen.keypad(0); curses.echo()
-    curses.endwin()
-    return
+    stdscr.addstr(5,  2, '======================================')
+    stdscr.addstr(6,  2, '#         Pan Tilt Controls          #')
+    stdscr.addstr(7,  2, '# URL: ssh://192.168.0.177:8022      #')
+    stdscr.addstr(8,  2, '#                                    #')
+    stdscr.addstr(9,  2, '#                                    #')
+    stdscr.addstr(10, 2, '#                Up                  #')
+    stdscr.addstr(11, 2, '#           Left    Right            #')
+    stdscr.addstr(12, 2, '#               Down                 #')
+    stdscr.addstr(13, 2, '#                                    #')
+    stdscr.addstr(14, 2, '#                                    #')
+    stdscr.addstr(15, 2, '#                                    #')
+    stdscr.addstr(16, 2, '# URL: http://192.168.0.177:8081     #')
+    stdscr.addstr(17, 2, '# URL: http://24.229.161.67:8081     #')
+    stdscr.addstr(18, 2, '======================================')
 
-if __name__ == '__main__':
+    stdscr.refresh()
+    stdscr.getkey()
+
     try:
-        main()
-    except KeyboardInterrupt:
-        print('\nCanceled by User.')
+        while True:
+            char = stdscr.getch()
+            if char == ord('q'):
+                break
+            if char == ord('d'):
+                pass
+            elif char == curses.KEY_RIGHT:
+                stdscr.addstr(0, 0, 'right')
+            elif char == curses.KEY_LEFT:
+                stdscr.addstr(0, 0, 'left')
+            elif char == curses.KEY_UP:
+                stdscr.addstr(0, 0, 'up')
+            elif char == curses.KEY_DOWN:
+                stdscr.addstr(0, 0, 'down')
+            else:
+                stdscr.addstr(0, 0, 'Not currently supported')
+    finally:
+        # Shutdown cleanly
+        curses.nocbreak(); stdscr.keypad(0); curses.echo()
+        curses.endwin()
+
+    return
+    
+
+wrapper(main)
+
+#if __name__ == '__main__':
+#    try:
+#        main()
+#        print(f"End main")
+#    except KeyboardInterrupt:
+#        print('\nCanceled by User.')
