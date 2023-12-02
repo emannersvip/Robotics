b6e5c759f9b22c51857a61df180479ec53f44f48
Edson Manners <edson.manners@gmail.com>
2023-12-02 17:25:41 +0000

    Sunfounder RC1

--- PiCamera/eom_pantilt_sunfounder.py
+++ PiCamera/eom_pantilt_sunfounder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# https://docs.python.org/3/howto/curses.html
 import curses
 
 from curses import wrapper
@@ -7,16 +8,11 @@ from servo import Servo
 from time import sleep
 
 pan  = Servo(pin=13, max_angle=90, min_angle=-90)
-#pan  = Servo(pin=13)
 tilt = Servo(pin=12, max_angle=30, min_angle=-90)
-#tilt = Servo(pin=12)
 
 def main(stdscr):
-    #while True:
-    #    print(f"Bob is here")
-    #    pan.set_angle(10)
-    #    sleep(2)
-    #    tilt.set_angle(10)
+    pan_angle = 0
+    tilt_angle = 0
     stdscr.clear()
 
     stdscr.addstr(1,  2, 'Press     to QUIT' )
@@ -25,7 +21,7 @@ def main(stdscr):
 
     stdscr.addstr(5,  2, '======================================')
     stdscr.addstr(6,  2, '#         Pan Tilt Controls          #')
-    stdscr.addstr(7,  2, '# URL: ssh://192.168.0.177:8022      #')
+    stdscr.addstr(7,  2, '# URL: ssh://192.168.68.24:22        #')
     stdscr.addstr(8,  2, '#                                    #')
     stdscr.addstr(9,  2, '#                                    #')
     stdscr.addstr(10, 2, '#                Up                  #')
@@ -34,8 +30,8 @@ def main(stdscr):
     stdscr.addstr(13, 2, '#                                    #')
     stdscr.addstr(14, 2, '#                                    #')
     stdscr.addstr(15, 2, '#                                    #')
-    stdscr.addstr(16, 2, '# URL: http://192.168.0.177:8081     #')
-    stdscr.addstr(17, 2, '# URL: http://24.229.161.67:8081     #')
+    stdscr.addstr(16, 2, '# URL: http://192.168.68.24:8000     #')
+    stdscr.addstr(17, 2, '# URL: http://24.229.161.67:8000     #')
     stdscr.addstr(18, 2, '======================================')
 
     stdscr.refresh()
@@ -50,12 +46,24 @@ def main(stdscr):
                 pass
             elif char == curses.KEY_RIGHT:
                 stdscr.addstr(0, 0, 'right')
+                pan_angle = pan_angle - 1
+                pan.set_angle(pan_angle)
+                print(f"Pan Angle: {pan_angle}")
             elif char == curses.KEY_LEFT:
                 stdscr.addstr(0, 0, 'left')
+                pan_angle = pan_angle + 1
+                pan.set_angle(pan_angle)
+                print(f"Pan Angle: {pan_angle}")
             elif char == curses.KEY_UP:
                 stdscr.addstr(0, 0, 'up')
+                tilt_angle = tilt_angle - 1
+                tilt.set_angle(tilt_angle)
+                print(f"Tilt Angle: {tilt_angle}")
             elif char == curses.KEY_DOWN:
                 stdscr.addstr(0, 0, 'down')
+                tilt_angle = tilt_angle + 1
+                tilt.set_angle(tilt_angle)
+                print(f"Tilt Angle: {tilt_angle}")
             else:
                 stdscr.addstr(0, 0, 'Not currently supported')
     finally:
