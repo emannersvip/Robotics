34de97d3c032db60e097640e4565261b03241c15
Edson Manners <edson.manners@gmail.com>
2023-12-31 06:50:36 +0000

    Updates

--- PiCamera/eom_web_cam_control.py
+++ PiCamera/eom_web_cam_control.py
@@ -70,7 +70,7 @@ def down():
     return render_template('index.html')
 
 def main():
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 if __name__ == '__main__':
     main()
