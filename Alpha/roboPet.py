085a4eb7bb7f3f4d91836ef4cadac3ae85d9b1d7
Edson Manners <edson.manners@gmail.com>
2026-06-18 17:30:32 -0500

    Updates

--- Alpha/roboPet.py
+++ Alpha/roboPet.py
@@ -6,43 +6,88 @@
 import curses
 import cv2
 import logging
+import numpy as np
+import os
 import time
 
 import pycreate2
 
-# Print CV2 version
+# Set environment variables
+os.environ['QT_QPA_PLATFORM']='xcb'
+
+# Print CV2 version and load a cascade classifier for object detection
 print(cv2.__version__)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 logfile='/home/emanners/Documents/Git/Robotics/Alpha/sensor.log'
 #logfile='/home/emanners/Code/Robotics/iRobotCreate2/sensor.log'
 logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s %(message)s')
-logging.info('============== Logs ==============')
+logging.info("\n")
+logging.info('\n============== RoboPet Logging Started ==============')
 
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(True)
+logging.info('Curses: curses initialized...')
 
 if __name__ == "__main__":
     # Create a Create2 bots settings
     port = '/dev/ttyUSB0'
     baud = { 'default': 115200 }
+    logging.info('PyCreate: Port set to %s and baud rate set to %d', port, baud['default'])
 
     # Create video
     cap = cv2.VideoCapture(0)
+    logging.info('CV: Camera initialized for CV')
     ret, frame = cap.read()
     if ret:
         screen.addstr(0, 0, 'Camera working correctly')
         msg=f"Frame shape: {frame.shape}"
         screen.addstr(1,0, msg)
+        cv2.imshow('Captured', frame)
+        logging.info('CV: Frame Captured')
+        cv2.imwrite('Captured1.png', frame)
+        logging.info('CV: Frame saved to file')
+        # Load an image from a file
+        img = cv2.imread('Captured1.png')
+        logging.info('CV: Image loaded for facial recognition "FR"')
+        # Convert image to grayscale
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect faces in the image
+        faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        # Iterate over the faces and draw rectangles around them
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255,0), 2)
+        # Display the output image
+        cv2.imshow('Output Image', img)
+        logging.info('CV: Displaying image with FR squares')
+        cv2.waitKey(0)
+        #cv2.destroyWindow('Captured')
+    else:
+        msg=f"Failed to capture image."
+        screen.addstr(1,20, msg)
+
+    logging.info('CV: Cleanly releasing capture device')
     cap.release()
-    img = cv2.imread('/home/emanners/Pictures/Screenshots/Screenshot From 2026-02-22 07-40-18.png')
-    cv2.imshow('Output', img)
-    cv2.waitKey(0)
+    #img = cv2.imread('/home/emanners/Pictures/Screenshots/Screenshot From 2026-02-22 07-40-18.png')
+    #cv2.imshow('Output', img)
+    #cv2.waitKey(0)
+    logging.info('CV: CV2 cleanup and window destruction')
     cv2.destroyAllWindows()
 
     # Create a Create 2
-    bot = pycreate2.Create2(port=port, baud=baud['default'])
+    try:
+        bot = pycreate2.Create2(port=port, baud=baud['default'])
+    except Exception as e:
+        logging.info('PyCreate - Exception: %s', e)
+        logging.info('PyCreate - Exception: No PyCreate Bot attached. Shutting down cleanly.')
+        screen.addstr(20, 0, 'No PyCreate Bot attached.')
+        curses.nocbreak(); screen.keypad(0); curses.echo()
+        curses.endwin()
+        time.sleep(0.5)
+        os._exit(1)
+
     # Start the Create 2
     bot.start()
     # Put the Create2 into 'safe' mode so we can drive it
@@ -105,7 +150,7 @@ if __name__ == "__main__":
                 bot.drive_direct(25,-25)
                 time.sleep(2.0)
                 bot.drive_stop()
-    finally:
+    except Exception as e:
         # Shut down cleanly
         curses.nocbreak(); screen.keypad(0); curses.echo()
         curses.endwin()
